d8c3a09cd631e819be19881a1f0a54ab0583b351
Mujahid Ali <magg2761@gmail.com>
2017-11-15 00:17:53 +0530

    Adding Intent

--- app/src/main/java/com/example/n5050/instatrack/MainActivity.java
+++ app/src/main/java/com/example/n5050/instatrack/MainActivity.java
@@ -5,20 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnSubmit;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnSubmit=(Button) findViewById(R.id.btnGetOTP);
+        textView=(TextView) findViewById(R.id.textID1);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),OtpAuthLogin.class);
-                startActivity(intent);
+                Intent intent1=new Intent(getApplicationContext(),OtpAuthLogin.class);
+                startActivity(intent1);
                 finish();
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2=new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent2);
             }
         });
 
