442d49728a5834b66774eced41e9893f39d27fa3
Mujahid Ali <magg2761@gmail.com>
2017-11-15 00:27:56 +0530

    Adding Idea files

--- app/src/main/java/com/example/n5050/instatrack/SignUpActivity.java
+++ app/src/main/java/com/example/n5050/instatrack/SignUpActivity.java
@@ -1,29 +1,84 @@
 package com.example.n5050.instatrack;
 
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button;
+    EditText etPhone;
+    ProgressDialog dialog;
+    String otp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        button=(Button) findViewById(R.id.btnSignUpOtp);
+        etPhone=(EditText)findViewById(R.id.etPhoneSignUp);
+        button.setOnClickListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    @Override
+    public void onClick(View view) {
+        String url = Config.baseUrl + "signUpOtp.php";
+        if(etPhone.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Please Enter Mobile Number",Toast.LENGTH_LONG).show();
+        }else{
+            dialog= ProgressDialog.show(SignUpActivity.this,"","Please Wait...",false,false);
+            StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        dialog.dismiss();
+                        Intent intentConfirmOTP = new Intent(getApplicationContext(), SignUpOTP.class);
+                        startActivity(intentConfirmOTP);
+                        finish();
+                    } else if (response.equals("fail")) {
+                        dialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Some error occured", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    dialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "Check Your Internet connection", Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> param=new HashMap<>();
+                    param.put("mobile",etPhone.getText().toString().trim());
+                    return param;
+                }
+            };
+            
+        }
+
+
+
+
+    }
 }
--- app/src/main/java/com/example/n5050/instatrack/SignUpOTP.java
+++ app/src/main/java/com/example/n5050/instatrack/SignUpOTP.java
@@ -1,13 +1,24 @@
 package com.example.n5050.instatrack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpOTP extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_otp);
+        button=(Button) findViewById(R.id.btnCheckOtp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentOTP=new Intent(getApplicationContext(),SignUpUserName.class);
+                startActivity(intentOTP);
+            }
+        });
     }
 }
